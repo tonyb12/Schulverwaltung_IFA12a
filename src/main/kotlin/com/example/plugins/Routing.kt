64a5644486b2eef1aeb20b374055c672bad662dc
Nico Hammon <n.hammon@bsh-ag.de>
2023-02-15 13:20:10 +0100

    [ISSUE] #6 Refactoring of Routing

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,7 +1,8 @@
 package com.example.plugins
 
-import com.example.database.DAOFacadeImpl
-import com.example.model.Student
+import com.example.controllers.StudentController
+import com.example.controllers.interfaces.IStudentController
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -11,24 +12,39 @@ import java.io.File
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        get("/students/{id}"){
-            val dao = DAOFacadeImpl()
-            val id = call.parameters["id"]
-            if (id != null) {
-                val student: Student? = dao.getStudent(id.toInt())
-                student ?.let {
-                    call.respond(VelocityContent("templates/students.vm", mapOf("student" to student)));
-                }
-            } else {
-
-            }
-        }
         static("assets") {
             staticRootFolder = File("src/main/resources/assets")
             files(".")
+        }
+        students(StudentController())
+    }
+}
+fun Routing.students(studentController: IStudentController) {
+    route("/students") {
+        get {
+            studentController.getAll()
+        }
+        get("/{id}") {
+            val idString = call.parameters["id"]
+            if (idString == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+                //TODO("Message needed?)
+            }
+            var id = 0
+            try {
+                id = idString.toInt()
+            } catch (ex: NumberFormatException) {
+                call.respond(HttpStatusCode.BadRequest, "Id must be a number")
+                return@get
+            }
+            val student = studentController.getById(id)
+            if (student == null) {
+                call.respond(HttpStatusCode.NotFound)
+                return@get
+            }
+            call.respond(VelocityContent("templates/students.vm", mapOf("student" to student)));
+
         }
     }
 }
