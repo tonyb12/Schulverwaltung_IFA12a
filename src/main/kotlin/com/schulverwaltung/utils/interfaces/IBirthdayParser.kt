6767db7b81bde4cdd396aa52edcdc12966994eb8
Nico Hammon <nicohammonofficial@gmail.com>
2023-03-17 07:59:22 +0100

    [ISSUE] #32 #2 Added documentation for the BirthdayParser and tests

--- src/main/kotlin/com/schulverwaltung/utils/interfaces/IBirthdayParser.kt
+++ src/main/kotlin/com/schulverwaltung/utils/interfaces/IBirthdayParser.kt
@@ -1,5 +1,19 @@
 package com.schulverwaltung.utils.interfaces
 
+import java.time.format.DateTimeParseException
+import kotlin.jvm.Throws
+
+/**
+ * BirthdayParser Interface
+ */
 interface IBirthdayParser {
+    /**
+     * Parses a date in the format "dd.MM.yyyy" to a String in the format "yyyyMMdd"
+     *
+     * @param date as String in the format "dd.MM.yyyy"
+     * @return Date as a String in the format "yyyyMMdd"
+     * @throws DateTimeParseException
+     */
+    @Throws(DateTimeParseException::class)
     fun parse(date: String): String
 }
