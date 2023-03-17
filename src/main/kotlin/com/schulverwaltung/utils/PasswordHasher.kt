8b45930b8e5a0c5dee399f8d627663fd71b56687
Nico Hammon <nicohammonofficial@gmail.com>
2023-03-17 08:01:09 +0100

    [ISSUE] #32 #2 Added documentation for the PasswordHasher and tests

--- src/main/kotlin/com/schulverwaltung/utils/PasswordHasher.kt
+++ src/main/kotlin/com/schulverwaltung/utils/PasswordHasher.kt
@@ -3,11 +3,27 @@ package com.schulverwaltung.utils
 import com.schulverwaltung.utils.interfaces.IPasswordHasher
 import org.mindrot.jbcrypt.BCrypt
 
+/**
+ * PasswordHasher
+ */
 class PasswordHasher : IPasswordHasher {
+    /**
+     * Hash password
+     *
+     * @param password the password to hash
+     * @return the hashed password
+     */
     override fun hashPassword(password: String): String {
         return BCrypt.hashpw(password, BCrypt.gensalt())
     }
 
+    /**
+     * Check that a plaintext password matches a previously hashed one
+     *
+     * @param password to verify
+     * @param hash to check against
+     * @return true if the passwords match, false otherwise
+     */
     override fun verifyPassword(password: String, hash: String): Boolean {
         return BCrypt.checkpw(password, hash)
     }
--- src/main/kotlin/com/schulverwaltung/utils/interfaces/IPasswordHasher.kt
+++ src/main/kotlin/com/schulverwaltung/utils/interfaces/IPasswordHasher.kt
@@ -1,6 +1,23 @@
 package com.schulverwaltung.utils.interfaces
 
+/**
+ * PasswordHasher Interface
+ */
 interface IPasswordHasher {
+    /**
+     * Hash password
+     *
+     * @param password the password to hash
+     * @return the hashed password
+     */
     fun hashPassword(password: String): String
+
+    /**
+     * Verify password
+     *
+     * @param password password to verify
+     * @param hash the previously hashed password
+     * @return true if the passwords match, false otherwise
+     */
     fun verifyPassword(password: String, hash: String): Boolean
 }
