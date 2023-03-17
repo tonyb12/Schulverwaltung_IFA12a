abc3fefcc1494706cf323dc900b07bf0dba8e0b8
Nico Hammon <nicohammonofficial@gmail.com>
2023-03-17 07:55:44 +0100

    [ISSUE] #32 Added MultiException-Class

--- src/main/kotlin/com/schulverwaltung/MultiException.kt
+++ src/main/kotlin/com/schulverwaltung/MultiException.kt
@@ -1,4 +1,11 @@
 package com.schulverwaltung
 
-class MultiException {
+class MultiException : Exception {
+    val exceptionList: List<Exception>
+    override val message: String
+    constructor(exceptionList: List<Exception>) {
+        this.exceptionList = exceptionList
+        val tmp =  exceptionList.map { it }.toString()
+        this.message = tmp
+    }
 }
